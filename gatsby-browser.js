c005b59386bc403b7f242aa6de1893c2863249e7
iammrvilla <3463688+mrvilla@users.noreply.github.com>
2021-05-19 21:41:47 +0200

    adjust prettierrc file and test gatsby-browser

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,3 +5,9 @@
  */
 
 // You can delete this file if you're not using it
+
+// exports.onPreRouteUpdate = ({ location, prevLocation, action }) => {
+//     console.log("location to", location.pathname,)
+//     console.log("prevLocation from", prevLocation ? prevLocation.pathname : null)
+//     console.log("action from", { location, prevLocation, action })
+// }
