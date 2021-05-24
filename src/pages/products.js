e4de55f552d38d900c9ac83c0fbf671be64c7cf5
iammrvilla <3463688+mrvilla@users.noreply.github.com>
2021-05-25 01:21:49 +0200

    add private product feature for logged users

--- src/pages/products.js
+++ src/pages/products.js
@@ -2,41 +2,65 @@ import React from 'react'
 import Img from "gatsby-image"
 import { graphql, Link } from 'gatsby'
 import Layout from "../components/layout"
+import netlifyIdentity from 'netlify-identity-widget' 
 
-const Products = ({data: {allContentfulProduct}}) => {
-    return (
-        <Layout>
-            <h2>Cyberpunk Posters</h2>
-            <div style={{
-                display: 'flex', 
-                justifyContent: 'space-around', 
-                flexWrap: 'wrap', 
-                columnGap: '60px',
-                rowGap: '40px',
-                }}>
-                {allContentfulProduct.edges.map(({node: product}) => (
-                    <div key={product.id} style={{
-                        display: 'flex', 
-                        justifyContent: 'center',
-                        flexDirection: 'column',
-                        alignItems: 'center',
-                        }}>
-                        <Link 
-                        to={`/products/${product.slug}`} 
-                        style={{color: '#663399', textDecoration: 'none'}}>
-                            <h3>{product.name} • {''}<span style={{color: '#f904ac'}}>€{product.price}</span></h3>
-                            
-                        </Link>
-                        <Img 
-                        fluid={product.image.fluid}
-                        alt={product.image.name}
-                        style={{width: 200}}
-                        />
-                    </div>
-                ))}
-            </div>
-        </Layout>
-    )
+class Products extends React.Component {
+    state = {
+        products: []
+    }
+    componentDidMount() {
+        this.getProducts()
+        netlifyIdentity.on('login', user => this.getProducts(user))
+        netlifyIdentity.on('logout', () => this.getProducts())
+    }
+
+    getProducts = user => {
+        console.log('current user', user)
+        const allProducts = this.props.data.allContentfulProduct.edges
+
+        const products = netlifyIdentity.currentUser() !== null ? 
+        allProducts : 
+        allProducts.filter(({node: product}) => !product.private)
+        this.setState({products})
+    }
+    
+    render() {
+        const {products} = this.state
+        return (
+            <Layout>
+                <h2>Cyberpunk Posters</h2>
+                <div style={{
+                    display: 'flex', 
+                    justifyContent: 'space-around', 
+                    flexWrap: 'wrap', 
+                    columnGap: '60px',
+                    rowGap: '40px',
+                    }}>
+                    {products.map(({node: product}) => (
+                        <div key={product.id} style={{
+                            display: 'flex', 
+                            justifyContent: 'center',
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                            }}>
+                            <Link 
+                            to={`/products/${product.slug}`} 
+                            style={{color: '#663399', textDecoration: 'none'}}>
+                                <h3>{product.name} • {''}<span style={{color: '#f904ac'}}>€{product.price}</span></h3>
+                                
+                            </Link>
+                            <Img 
+                            fluid={product.image.fluid}
+                            alt={product.image.name}
+                            style={{width: 200}}
+                            />
+                        </div>
+                    ))}
+                </div>
+            </Layout>
+        )
+    }
+    
 }
 
 export const query = graphql`
@@ -48,6 +72,7 @@ export const query = graphql`
                 name
                 slug
                 price
+                private
                 image {
                     fluid(maxWidth: 400) {
                         ...GatsbyContentfulFluid_tracedSVG 
